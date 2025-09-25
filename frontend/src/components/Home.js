66c5ba22c8a2fb1bf4c4f77224aaa19de7eb4469
molison <sonia.molina@innobadev.altia.es>
2025-09-25 15:42:11 +0200

    Continue with dApp

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import MoneyIcon from "@mui/icons-material/MonetizationOnOutlined";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 1, mb: 2 }}>
+        <Typography variant="h3">CodeWallet</Typography>
+        <MoneyIcon fontSize="large" />
+      </Box>
+      <Typography>Bienvenido a tu Wallet</Typography>
+      <Button variant="contained" onClick={() => navigate("/yourwallet")}>
+        Ir a tu Wallet
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
